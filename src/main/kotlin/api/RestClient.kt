0632b34e1ea8bea356ff371fadf4cb8af84ba035
Yuriy Tumakha <Yuriy.Tumakha@iplato.com>
2021-06-17 12:57:09 +0100

    URI(url)

--- src/main/kotlin/api/RestClient.kt
+++ src/main/kotlin/api/RestClient.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import java.net.URI
-import java.net.URL
 import java.net.http.HttpClient
 import java.net.http.HttpClient.Redirect
 import java.net.http.HttpClient.Version
